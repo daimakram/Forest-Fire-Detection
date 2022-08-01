f027e31140226533c04a5e6686b32eadb75b3146
daimakram <85197435+daimakram@users.noreply.github.com>
2022-08-01 14:57:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -269,7 +269,7 @@ def main(args):
 
     print("Start training")
     start_time = time.time()
-    for epoch in range(args.start_epoch, args.epochs):
+    for epoch in range(0, 10):
         if args.distributed:
             sampler_train.set_epoch(epoch)
         train_stats = train_one_epoch(
